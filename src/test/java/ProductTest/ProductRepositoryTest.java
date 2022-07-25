5f69ef8ce5d1f180cdd7f457acce4997e5fc32f6
Danila Kalay <ChechikLud@gmail.com>
2022-07-25 11:27:49 +0300

    add RepositoryTest commit

--- src/test/java/ProductTest/ProductRepositoryTest.java
+++ src/test/java/ProductTest/ProductRepositoryTest.java
@@ -9,25 +9,25 @@ import org.junit.jupiter.api.Test;
 
 class ProductRepositoryTest {
     ProductRepository repository = new ProductRepository();
-    Product book1 = new Book(1, "Мать", 500, "Максим Горький");
-    Product book2 = new Book(2, "Ведьмак", 1500, "Анджей Сапковский");
-    Product book3 = new Book(3, "Сказки", 1000, "Александр Пушкин");
-    Product book4 = new Book(4, "Бородино", 500, "Михаил Лермонтов");
-    Product smartphone1 = new Smartphone(5, "Galaxy A5", 5000, "Samsung");
-    Product smartphone2 = new Smartphone(6, "RedmiNote 9", 14000, "Xiaomi");
-    Product smartphone3 = new Smartphone(7, "Iphone 9S", 25000, "Samsung");
+    Product product1 = new Book(1, "Мать", 500, "Максим Горький");
+    Product product2 = new Book(2, "Сказки", 1500, "Шарль Перро");
+    Product product3 = new Book(3, "Сказки", 1000, "Александр Пушкин");
+    Product product4 = new Book(4, "Бородино", 500, "Михаил Лермонтов");
+    Product product5 = new Smartphone(5, "Galaxy A5", 5000, "Samsung");
+    Product product6 = new Smartphone(6, "RedmiNote 9", 14000, "Xiaomi");
+    Product product7 = new Smartphone(7, "Iphone 9S", 25000, "Samsung");
 
 
     @Test
     void save() {
-        repository.save(book1);
-        repository.save(book2);
-        repository.save(book3);
-        repository.save(book4);
-        repository.save(smartphone1);
-        repository.save(smartphone2);
-        repository.save(smartphone3);
-        Product[] expected = {book1, book2, book3, book4, smartphone1, smartphone2, smartphone3};
+        repository.save(product1);
+        repository.save(product2);
+        repository.save(product3);
+        repository.save(product4);
+        repository.save(product5);
+        repository.save(product6);
+        repository.save(product7);
+        Product[] expected = {product1, product2, product3, product4, product5, product6, product7};
         Product[] actual = repository.getProducts();
         Assertions.assertArrayEquals(expected, actual);
 
@@ -36,15 +36,15 @@ class ProductRepositoryTest {
 
     @Test
     void removeById() {
-        repository.save(book1);
-        repository.save(book2);
-        repository.save(book3);
-        repository.save(book4);
-        repository.save(smartphone1);
-        repository.save(smartphone2);
-        repository.save(smartphone3);
-        repository.removeById(book4.getId());
-        Product[] expected = {book1, book2, book3, smartphone1, smartphone2, smartphone3};
+        repository.save(product1);
+        repository.save(product2);
+        repository.save(product3);
+        repository.save(product4);
+        repository.save(product5);
+        repository.save(product6);
+        repository.save(product7);
+        repository.removeById(product4.getId());
+        Product[] expected = {product1, product2, product3,  product5, product6, product7};
 
         Product[] actual = repository.getProducts();
         Assertions.assertArrayEquals(expected, actual);
